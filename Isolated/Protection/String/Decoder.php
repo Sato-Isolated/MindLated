00783501ce1201fe56335bca1391f39bffe8bd76
Isolated <ismyskyllz@gmail.com>
2020-05-05 17:39:36 +0200

    Create Decoder.php

---
+++ Isolated/Protection/String/Decoder.php
@@ -0,0 +1,11 @@
+<?php
+$hex = $_GET['string'];
+function hexToStr($hex){
+    $string='';
+    for ($i=0; $i < strlen($hex)-1; $i+=2){
+        $string .= chr(hexdec($hex[$i].$hex[$i+1]));
+    }
+    return $string;
+}
+$str = hexToStr($hex);
+echo "{$str}\n";
