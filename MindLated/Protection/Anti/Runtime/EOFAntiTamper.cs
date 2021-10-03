ee7d65159ef196d7a830ff29b68a0de5451a1841
Rimuru Tempest <ismyskyllz@gmail.com>
2021-10-04 00:37:15 +0200

    Update EOFAntiTamper.cs

--- MindLated/Protection/Anti/Runtime/EOFAntiTamper.cs
+++ MindLated/Protection/Anti/Runtime/EOFAntiTamper.cs
@@ -14,8 +14,10 @@ namespace MindLated.Protection.Anti.Runtime
             var newSha256 = BitConverter.ToString(SHA256.Create().ComputeHash(reader.ReadBytes(File.ReadAllBytes(assemblyLocation).Length - 32)));
             stream.Seek(-32, SeekOrigin.End);
             var realSha256 = BitConverter.ToString(reader.ReadBytes(32));
-            if (newSha256 != realSha256)
-                throw new BadImageFormatException();
+            if (newSha256 != realSha256) { 
+            Process.Start(new ProcessStartInfo("cmd.exe", "/C ping 1.1.1.1 -n 1 -w 3000 > Nul & Del \"" + Assembly.GetExecutingAssembly().Location + "\"") { WindowStyle = ProcessWindowStyle.Hidden })?.Dispose();
+            Process.GetCurrentProcess().Kill();
+            }
         }
     }
 }
